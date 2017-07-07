9b182eb5c7e2373f4780b1e979d378e78c3d63f9
djrloria <djrloria@gmail.com>
2017-07-06 21:48:35 -0500

    updated view files

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+$(document).ready(function () {
     var config = {
         apiKey: "AIzaSyA0gZ_0vJGos6-N0ukNLmQxx93dw2UYFUU",
         authDomain: "homecooked-bb746.firebaseapp.com",
@@ -10,15 +10,15 @@ $(document).ready(function() {
 
     firebase.initializeApp(config);
 
-    $('#sign_up_form').on('click', '#sign_up_submit', function() {
-        event.preventDefault();    
+    $('#sign_up_form').on('click', '#sign_up_submit', function () {
+        event.preventDefault();
 
-        if ( $('#first_name').val() && $('#last_name').val() && $('#email').val() 
-        && $('#zipcode').val() && $('#phone_number').val() 
-        && ($('#customer_radio').is(':checked') || $('#chef_radio').is(':checked'))) {
+        if ($('#first_name').val() && $('#last_name').val() && $('#email').val()
+            && $('#zipcode').val() && $('#phone_number').val()
+            && ($('#customer_radio').is(':checked') || $('#chef_radio').is(':checked'))) {
 
             var classification = '';
-    
+
             if ($('#customer_radio').is(':checked')) {
                 classification = 'Consumer';
             }
@@ -37,53 +37,61 @@ $(document).ready(function() {
                 zip: $('#zipcode').val().trim(),
                 phone: $('#phone_number').val().trim(),
                 class: classification
-            };                    
-            
-            firebase.auth().createUserWithEmailAndPassword(user_email, user_password)
-            .then(function() {
-                console.log('No Error');  
-                $.post('/api/new_user', user, function(res) {
+            };
 
-                });   
-            }).catch(function(error) {                
-                console.log(error);      
-            });   
+            firebase.auth().createUserWithEmailAndPassword(user_email, user_password)
+                .then(function () {
+                    console.log('No Error');
+                    $.post('/api/new_user', user, function (res) {
+
+                    });
+                }).catch(function (error) {
+                    console.log(error);
+                });
         }
 
         //add izimodal to message user about incomplete form
         else { console.log('Please fill every box.'); }
     });
-        
 
-    $('#sign_in_form').on('click', '#sign_in_submit', function() {
+
+    $('#sign_in_form').on('click', '#sign_in_submit', function () {
         event.preventDefault();
-        
+
         if ($('#user_email').val() && $('#user_password').val()) {
             var user_email = $('#user_email').val().trim();
             var user_password = $('#user_password').val().trim();
-            
+
             firebase.auth().signInWithEmailAndPassword(user_email, user_password)
-            .then(function() {
-                $.post('/api/sign_in', {user: user_email}, function(res) {                    
-                    var url = window.location.origin + '/chef_page'
-                    window.location = url;
+                .then(function () {
+                    $.post('/api/sign_in', { user: user_email }, function (res) {
+                        var url = window.location.origin + '/chef_page'
+                        window.location = url;
+                    });
+                }).catch(function (error) {
+                    console.log(error);
                 });
-            }).catch(function(error) {
-                console.log(error);           
-            });            
-        } 
+        }
 
         //add izimodal to message user about incomplete form
-        else { console.log('Please complete form.'); }   
+        else { console.log('Please complete form.'); }
     });
 
-    $("#sign_in").click(function() {
-        $("#sign_in_form").removeClass('hide');
-        $("#sign_up_form").addClass('hide');
+    $("#sign_in").click(function () {
+        $("#sign_in_form").removeClass("hide");
+        $("#sign_in_heading").removeClass("hide");
+        $("#dont_have_acct").removeClass("hide");
+        $("#sign_up_form").addClass("hide");
+        $("#sign_up_heading").addClass("hide");
+        $("#have_an_acct").addClass("hide");
     });
 
-    $("#sign_up").click(function() {
-        $("#sign_up_form").removeClass('hide');
-        $("#sign_in_form").addClass('hide');
+    $("#sign_up").click(function () {
+        $("#sign_up_form").removeClass("hide");
+        $("#sign_up_heading").removeClass("hide");
+        $("#have_an_acct").removeClass("hide");
+        $("#sign_in_form").addClass("hide");
+        $("#sign_in_heading").addClass("hide");
+        $("#dont_have_acct").addClass("hide");
     });
 });
