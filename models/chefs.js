3b79d7075c787e923a4ac885b1fff34c876f8d76
djrloria <djrloria@gmail.com>
2017-07-06 20:54:59 -0500

    corrected files

--- models/chefs.js
+++ models/chefs.js
@@ -16,21 +16,7 @@ module.exports = function(sequelize , DataTypes) {
             type: DataTypes.INTEGER,
             allowNull: false
         },
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        picture: {
-=======
         picture_url: {
-=======
-        picture_url: {
-=======
-        picture: {
->>>>>>> update chefs.js
->>>>>>> updated models and package.json
-=======
-        picture_url: {
->>>>>>> update chefs.js
             type: DataTypes.STRING,
             allowNull: true
         }
